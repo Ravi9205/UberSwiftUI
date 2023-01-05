07e219b68e6860c243927c2d7c22641940d64764
Ravi dwivedi <ravi.dwivedi9205@gmail.com>
2023-01-05 12:16:35 +0530

    Initial Commit

---
+++ UberSwiftUI/UberSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberSwiftUIApp.swift
+//  UberSwiftUI
+//
+//  Created by Ravi Dwivedi on 05/01/23.
+//
+
+import SwiftUI
+
+@main
+struct UberSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
